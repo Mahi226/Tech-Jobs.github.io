1ab5d240f25c1566821105fd194198dee4a89c28
MAHESH DUGGILLA <136039433+Mahi226@users.noreply.github.com>
2024-04-27 15:13:47 +0530

    Add files via upload

---
+++ Job_portal_Home_page.js
@@ -0,0 +1,12 @@
+const bar = document.getElementById("mobile")
+const nav = document.getElementById("navbar")
+
+
+
+if(bar) {
+	bar.addEventListener("click",()=>{
+		nav.classList.add("active");
+	})
+}
+
+
